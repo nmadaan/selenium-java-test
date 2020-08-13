5b77269e9b9f2f7edb6745a433a91f50b766b524
nmadaan <madaan.neha@gmail.com>
2020-08-13 15:12:14 +0530

    Update FirstTest.java

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
