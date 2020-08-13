42997915f3032fa4ffb722c5e77c40d31b49d690
nmadaan <madaan.neha@gmail.com>
2020-08-13 16:10:38 +0530

    Update and rename FirstTest.java to ThreadLocalDemo.java

---
+++ src/test/java/ThreadLocalDemo.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/*****************************************************************************
+ * Author:      Onur Baskirt
+ * Description: This is the first Selenium TestNG test.
+ *              It opens swtestacademy homepage and prints and checks its title.
+*******************************************************************************/
+
+public class FirstTest {
+
+    public String testURL = "https://google.com";
+
+    @Test
+    public void firstTest () {
+        //Get page title
+	
+        long id = Thread.currentThread().getId();
+        System.out.println("Test Method firstTest . Thread id is: " + id);
+	WebDriver driver=LocalDriverManager.getDriver();
+	driver.get(testURL);
+	String path;
+        try {
+            File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            path = "./target/screenshots/" + "success_screenshots_" + formater.format(calendar.getTime()) + ".png";
+            FileUtils.copyFile(source, new File(path)); 
+        }
+        catch(IOException e) {
+            path = "Failed to capture screenshot: " + e.getMessage();
+        }
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Google", "Title assertion is passed!");
+    }
+
+     @Test
+     public void secondTest () {
+	long id = Thread.currentThread().getId();
+        System.out.println("Test Method secondTest . Thread id is: " + id);
+	WebDriver driver=LocalDriverManager.getDriver();
+	driver.get(testURL);
+        String path;
+        try {
+            File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            path = "./target/screenshots/" + "success_screenshots_" + formater.format(calendar.getTime()) + ".png";
+            FileUtils.copyFile(source, new File(path)); 
+        }
+        catch(IOException e) {
+            path = "Failed to capture screenshot: " + e.getMessage();
+        }
+        WebElement signtext = driver.findElement(By.id("gb_70"));
+	Assert.assertEquals("Sign in", signtext.getText());
+    }
+}
