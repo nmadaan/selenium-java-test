a0ec6d08b48b8d06f5104f3765ad54820f5ba5b4
nmadaan <madaan.neha@gmail.com>
2020-08-13 15:57:31 +0530

    Update LocalDriverFactory.java

--- src/test/java/LocalDriverFactory.java
+++ src/test/java/LocalDriverFactory.java
@@ -15,6 +15,11 @@ class LocalDriverFactory {
             return driver;
         }
         if (browserName.toLowerCase().contains("chrome")) {
+            System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            driver = new ChromeDriver(options);
             driver = new ChromeDriver();
             return driver;
         }
